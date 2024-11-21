c3bcc0a2aa14fb008c7f5673b0d3e8ced2042f35
Hassan Kandil <hassanosama@aucegypt.edu>
2024-11-21 20:12:37 +0000

    Added pipeline infra terraform files

---
+++ main.tf
@@ -0,0 +1,171 @@
+data "harvester_image" "img" {
+  display_name = var.img_display_name
+  namespace    = "harvester-public"
+}
+
+data "harvester_ssh_key" "mysshkey" {
+  name      = var.keyname
+  namespace = var.namespace
+}
+
+data "harvester_ssh_key" "lecturersshkey" {
+  name = var.lecturer_keyname
+  namespace = var.namespace
+}
+
+resource "random_id" "secret" {
+  byte_length = 5
+}
+
+resource "harvester_cloudinit_secret" "cloud-config" {
+  name      = "cloud-config-${random_id.secret.hex}"
+  namespace = var.namespace
+
+  user_data = templatefile("cloud-init.tmpl.yml", {
+      public_key_openssh = data.harvester_ssh_key.mysshkey.public_key
+      lecturer_public_key = data.harvester_ssh_key.lecturersshkey.public_key
+    })
+}
+
+resource "harvester_virtualmachine" "hostvm" {
+  
+  count = 1
+
+  name                 = "${var.username}-host-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  namespace            = var.namespace
+  restart_after_update = true
+
+  description = "Cluster Host Node"
+
+  cpu    = 2
+  memory = "4Gi"
+
+  efi         = true
+  secure_boot = false
+
+  run_strategy    = "RerunOnFailure"
+  hostname        = "${var.username}-host-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  reserved_memory = "100Mi"
+  machine_type    = "q35"
+
+  network_interface {
+    name           = "nic-1"
+    wait_for_lease = true
+    type           = "bridge"
+    network_name   = var.network_name
+  }
+
+  disk {
+    name       = "rootdisk"
+    type       = "disk"
+    size       = "10Gi"
+    bus        = "virtio"
+    boot_order = 1
+
+    image       = data.harvester_image.img.id
+    auto_delete = true
+  }
+
+  cloudinit {
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
+  }
+}
+
+resource "harvester_virtualmachine" "workervm" {
+  
+  count = var.vm_count
+
+  name                 = "${var.username}-worker-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  namespace            = var.namespace
+  restart_after_update = true
+
+  description = "Cluster Compute Node"
+
+  cpu    = 4
+  memory = "32Gi"
+
+  efi         = true
+  secure_boot = false
+
+  run_strategy    = "RerunOnFailure"
+  hostname        = "${var.username}-worker-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  reserved_memory = "100Mi"
+  machine_type    = "q35"
+
+  network_interface {
+    name           = "nic-1"
+    wait_for_lease = true
+    type           = "bridge"
+    network_name   = var.network_name
+  }
+
+  disk {
+    name       = "rootdisk"
+    type       = "disk"
+    size       = "25Gi"
+    bus        = "virtio"
+    boot_order = 1
+
+    image       = data.harvester_image.img.id
+    auto_delete = true
+  }
+
+  cloudinit {
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
+  }
+}
+
+
+resource "harvester_virtualmachine" "storagevm" {
+  
+  count = 1
+
+  name                 = "${var.username}-storage-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  namespace            = var.namespace
+  restart_after_update = true
+
+  description = "Cluster Storage Node"
+
+  cpu    = 4
+  memory = "8Gi"
+
+  efi         = true
+  secure_boot = false
+
+  run_strategy    = "RerunOnFailure"
+  hostname        = "${var.username}-worker-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  reserved_memory = "100Mi"
+  machine_type    = "q35"
+
+  network_interface {
+    name           = "nic-1"
+    wait_for_lease = true
+    type           = "bridge"
+    network_name   = var.network_name
+  }
+
+  disk {
+    name       = "rootdisk"
+    type       = "disk"
+    size       = "10Gi"
+    bus        = "virtio"
+    boot_order = 1
+
+    image       = data.harvester_image.img.id
+    auto_delete = true
+  }
+
+  disk {
+    name       = "datadisk"
+    type       = "disk"
+    size       = "200Gi"
+    bus        = "virtio"
+    boot_order = 2
+
+    auto_delete = true
+  }
+
+  cloudinit {
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
+  }
+}

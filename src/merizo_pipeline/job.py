2241116aa19b14f4b338c19e0a9d216d8615d019
Hassan Kandil <hassanosama@aucegypt.edu>
2024-12-06 19:44:39 +0000

    Added pyspark job for running merizo search and jupyter playground playbook

--- src/merizo_pipeline/job.py
+++ src/merizo_pipeline/job.py
@@ -1,15 +1,48 @@
-from dataeng.analysis import analyse_spark
-from pyspark import SparkContext
+from subprocess import Popen, PIPE
+from tempfile import NamedTemporaryFile
 from pyspark.sql import SparkSession
+import os
 
-spark = SparkSession.builder.appName("Britishlibrary").getOrCreate()
-sc = spark.sparkContext
+def run_merizo_search(file_tuple):
+    input_file_name, file_content = file_tuple
+    print(f"File Name: {input_file_name}")
+    # Create a temporary file to hold the content
+    with NamedTemporaryFile(delete=True, mode='wb') as temp_file:
+        temp_file.write(file_content)
+        temp_file_path = temp_file.name
+    
+    cmd = ['python3',
+           '/home/almalinux/merizo_search/merizo_search/merizo.py',
+           'easy-search',
+           temp_file_path,
+           '/home/almalinux/data/cath-4.3-foldclassdb',
+           input_file_name,
+           'tmp',
+           '--iterate',
+           '--output_headers',
+           '-d',
+           'cpu',
+           '--threads',
+           '1'
+          ]
 
-results = analyse_spark("/1510_1699", sc, "arms")
-data = []
-columns = ["year", "count"]
-for a in results.keys():
-    data.append([a, results[a]])
-df = spark.createDataFrame(data, columns).coalesce(1)
-df.show()
-df.write.option("header","true").mode("overwrite").csv("/1510_1699_sparksubmit")
+    print(f'STEP 1: RUNNING MERIZO: {" ".join(cmd)}')
+    p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    # Decode the byte output to string
+    print("Output:")
+    print(out.decode())  # Decode and print the standard output
+    
+    if err:
+        print("Error:")
+        print(err.decode())  # Decode and print the standard error
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("MerizoSearch").getOrCreate()
+    sc = spark.sparkContext
+
+    input_dir = "/UP000000625_83333_ECOLI_v4/"
+    file_rdd = sc.binaryFiles(input_dir + "*.pdb")
+    file_content_rdd = file_rdd.map(lambda x: (os.path.basename(x[0]), x[1]))
+
+    file_content_rdd.map(run_merizo_search).collect()

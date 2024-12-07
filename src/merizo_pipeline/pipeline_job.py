6c351dbf2b9844710c11ad9b917130baae9dba23
Hassan Kandil <hassanosama@aucegypt.edu>
2024-12-07 03:16:17 +0000

    Parallelized pipeline script using spark

---
+++ src/merizo_pipeline/pipeline_job.py
@@ -0,0 +1,68 @@
+from subprocess import Popen, PIPE
+from tempfile import NamedTemporaryFile
+from pyspark.sql import SparkSession
+import os
+
+
+def run_parser(input_file):
+    """
+    Run the results_parser.py over the hhr file to produce the output summary
+    """
+    search_file = input_file+"_search.tsv"
+    print("search_file: ", search_file)
+    cmd = ['python', './results_parser.py', search_file]
+    print(f'STEP 2: RUNNING PARSER: {" ".join(cmd)}')
+    p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    print(out.decode("utf-8"))
+
+
+def run_merizo_search(file_name, file_content):
+    print(f"File Name: {file_name}")
+    # Create a temporary file to hold the content
+    with NamedTemporaryFile(delete=True, mode='wb') as temp_file:
+        temp_file.write(file_content)
+        temp_file_path = temp_file.name
+        cmd = ['python3',
+           '/home/almalinux/merizo_search/merizo_search/merizo.py',
+           'easy-search',
+           temp_file_path,
+           '/home/almalinux/data/cath-4.3-foldclassdb',
+           file_name,
+           'tmp',
+           '--iterate',
+           '--output_headers',
+           '-d',
+           'cpu',
+           '--threads',
+           '1'
+          ]
+        print(f'STEP 1: RUNNING MERIZO: {" ".join(cmd)}')
+        p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        # Decode the byte output to string
+        print("Output:")
+        print(out.decode())  # Decode and print the standard output
+        
+        if err:
+            print("Error:")
+            print(err.decode())  # Decode and print the standard error
+
+
+def pipeline(file_tuple):
+    file_name, file_content = file_tuple
+    # STEP 1
+    run_merizo_search(file_name, file_content)
+    # STEP 2
+    run_parser(file_name)
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("MerizoSearch").getOrCreate()
+    sc = spark.sparkContext
+
+    input_dir = "/UP000000625_83333_ECOLI_v4/"
+    file_rdd = sc.binaryFiles(input_dir + "*.pdb")
+    file_rdd = file_rdd.sample(withReplacement=False, fraction=0.005)
+    file_content_rdd = file_rdd.map(lambda x: (os.path.basename(x[0]), x[1]))
+
+    file_content_rdd.map(pipeline).collect()

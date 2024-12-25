97afb7e0ce0bee683c94c5cb3690d6c665b77917
Hassan Kandil <hassanosama@aucegypt.edu>
2024-12-25 21:56:33 +0000

    Renamed merizo pipeline package

--- src/merizo_analysis/pipeline.py
+++ src/merizo_analysis/pipeline.py
@@ -91,6 +91,47 @@ def run_merizo_search(file_name, file_content):
             print(err.decode("utf-8"))  # Decode and print the standard 
 
 
+def read_parsed_file(file_name):
+    """
+    Reads a .parsed file and extracts:
+    1. A dictionary with 'cath_id' as keys and their counts as values.
+    2. The mean pLDDT value.
+    
+    Args:
+        file_path (str): Path to the .parsed file.
+        
+    Returns:
+        tuple: (dict of cath_id counts, mean pLDDT value)
+    """
+    cath_counts = {}
+    mean_plddt = 0
+
+    file_path = file_name + '.parsed'
+
+    if not os.path.exists(file_path):
+        return mean_plddt, cath_counts
+    
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        
+        # Extract mean pLDDT from the header
+        if len(lines) < 1:
+            return cath_counts, mean_plddt
+
+        first_line = lines[0]
+        if "mean plddt:" in first_line:
+            mean_plddt = float(first_line.split("mean plddt:")[1].strip())
+        
+        # Skip the header and process the data rows
+        for line in lines[2:]:  # Assuming data rows start from the 3rd line
+            if not line.strip():
+                continue # Ignore empty lines
+            cath_id, count = line.strip().split(',')
+            cath_counts[cath_id] = int(count)
+    
+    return mean_plddt, cath_counts
+
+
 def pipeline(file_tuple):
     file_name, file_content = file_tuple
     # STEP 1
@@ -98,5 +139,7 @@ def pipeline(file_tuple):
     # STEP 2
     run_parser(file_name)
     # STEP 3
+    mean_plddt, cath_counts_dict = read_parsed_file(file_name)
+    # STEP 4
     upload_analysis_outputs_to_hdfs(file_name)
-
+    return mean_plddt, cath_counts_dict 

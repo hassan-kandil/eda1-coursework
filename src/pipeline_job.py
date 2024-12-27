e055de0d4f085879accac9268559ee3143815c20
Hassan Kandil <hassanosama@aucegypt.edu>
2024-12-27 16:19:14 +0000

    Updated pipeline job script to include human dataset

--- src/pipeline_job.py
+++ src/pipeline_job.py
@@ -28,30 +28,39 @@ def write_df_to_hdfs_csv(df, hdfs_path, csv_file_name):
         print("Error:")
         print(err.decode("utf-8"))  # Decode and print the standard  
 
+def prepare_rdd(sc, input_dir):
+    file_rdd = sc.binaryFiles(input_dir + "*.pdb")
+    file_rdd = file_rdd.sample(withReplacement=False, fraction=0.001)
+    file_content_rdd = file_rdd.map(lambda x: (os.path.basename(x[0]), x[1]))
+    return file_content_rdd
+
 if __name__ == "__main__":
     spark = SparkSession.builder \
         .appName("MerizoPipeline") \
         .getOrCreate()
     sc = spark.sparkContext
 
-    input_dir = "/UP000000625_83333_ECOLI_v4/"
-    file_rdd = sc.binaryFiles(input_dir + "*.pdb")
-    # file_rdd = file_rdd.sample(withReplacement=False, fraction=0.001)
-    file_content_rdd = file_rdd.map(lambda x: (os.path.basename(x[0]), x[1]))
+    ecoli_dir = "/UP000000625_83333_ECOLI_v4/"
+    human_dir = "/UP000005640_9606_HUMAN_v4/"
+
+    ecoli_content_rdd = prepare_rdd(sc, ecoli_dir)
+    human_content_rdd = prepare_rdd(sc, human_dir)
 
     # Use aggregate to compute the result
     start_time = time.time()
-    result = file_content_rdd.map(pipeline).aggregate(zero_value, seq_op, comb_op)
+    ecoli_result = ecoli_content_rdd.map(pipeline).aggregate(zero_value, seq_op, comb_op)
+    human_result = human_content_rdd.map(pipeline).aggregate(zero_value, seq_op, comb_op)
 
     # Extract results
-    mean, population_std_dev, combined_dict = extract_results(result)
+    human_mean, human_population_std_dev, human_combined_dict = extract_results(human_result)
+    ecoli_mean, ecoli_population_std_dev, ecoli_combined_dict = extract_results(ecoli_result)
 
     end_time = time.time()
     print(f"Done with the pipeline in {end_time - start_time:.2f} seconds")  
 
     # Convert results to DataFrames
     stats_df = spark.createDataFrame(
-        [("human", mean, population_std_dev), ("ecoli", mean, population_std_dev)],
+        [("human", human_mean, human_population_std_dev), ("ecoli", ecoli_mean, ecoli_population_std_dev)],
         schema=StructType([
             StructField("organism", StringType(), True),
             StructField("mean plddt", FloatType(), True),
@@ -61,18 +70,28 @@ if __name__ == "__main__":
 
     stats_df.show()
 
-    dict_df = spark.createDataFrame(
-        [(key, value) for key, value in combined_dict.items()],
+    ecoli_dict_df = spark.createDataFrame(
+        [(key, value) for key, value in ecoli_combined_dict.items()],
         schema=StructType([
             StructField("cath_code", StringType(), True),
             StructField("count", IntegerType(), True),
         ])
     ).coalesce(1)
 
-    dict_df.show()
+    ecoli_dict_df.show()
+
+    human_dict_df = spark.createDataFrame(
+        [(key, value) for key, value in human_combined_dict.items()],
+        schema=StructType([
+            StructField("cath_code", StringType(), True),
+            StructField("count", IntegerType(), True),
+        ])
+    ).coalesce(1)
+
+    human_dict_df.show()
 
     write_df_to_hdfs_csv(stats_df, "/summary_outputs/", "pIDDT_means")
-    write_df_to_hdfs_csv(dict_df, "/summary_outputs/", "ecoli_cath_summary")
-
+    write_df_to_hdfs_csv(ecoli_dict_df, "/summary_outputs/", "ecoli_cath_summary")
+    write_df_to_hdfs_csv(human_dict_df, "/summary_outputs/", "human_cath_summary")
 
 

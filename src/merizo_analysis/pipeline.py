c4a27a075d79888fac73bb303a0224a12df5c4ed
Hassan Kandil <hassanosama@aucegypt.edu>
2025-01-05 03:11:46 +0000

    Reduced threads count in merizo to 1 + formatting

--- src/merizo_analysis/pipeline.py
+++ src/merizo_analysis/pipeline.py
@@ -6,62 +6,85 @@ import os
 from merizo_analysis.utils import delete_local_file, run_command
 from merizo_analysis.config import logger
 
+
 def upload_file_to_hdfs(local_file_path, hdfs_file_path):
-    hdfs_put_cmd = ['/home/almalinux/hadoop-3.4.0/bin/hdfs', 'dfs', '-put', local_file_path, hdfs_file_path]
+    hdfs_put_cmd = [
+        "/home/almalinux/hadoop-3.4.0/bin/hdfs",
+        "dfs",
+        "-put",
+        local_file_path,
+        hdfs_file_path,
+    ]
     logger.info(f'STEP 3: UPLOADING ANALYSIS OUTPUT TO HDFS: {" ".join(hdfs_put_cmd)}')
     run_command(hdfs_put_cmd)
 
+
 def upload_analysis_outputs_to_hdfs(file_name):
     # upload anaylsis output files to hdfs and clean local files
-    local_files_paths = [ file_name + '_segment.tsv', file_name + '_search.tsv', file_name + '.parsed']
-    hdfs_file_path = '/analysis_outputs/'
+    local_files_paths = [
+        file_name + "_segment.tsv",
+        file_name + "_search.tsv",
+        file_name + ".parsed",
+    ]
+    hdfs_file_path = "/analysis_outputs/"
     for local_file_path in local_files_paths:
         if not os.path.exists(local_file_path):
-            logger.info(f"File {local_file_path} does not exist. Skipping the upload to hdfs.")
+            logger.info(
+                f"File {local_file_path} does not exist. Skipping the upload to hdfs."
+            )
             continue
         upload_file_to_hdfs(local_file_path, hdfs_file_path)
         delete_local_file(local_file_path)
+
 
 def run_parser(input_file):
     """
     Run the results_parser.py over the tsv search file to produce the output summary
     """
-    search_file = input_file+"_search.tsv"
+    search_file = input_file + "_search.tsv"
     if not os.path.exists(search_file):
         logger.info(f"Search file {search_file} does not exist. Skipping the parser.")
         return
-    cmd = ['python3', '/home/almalinux/merizo_pipeline/merizo_analysis/results_parser.py', search_file]
+    cmd = [
+        "python3",
+        "/home/almalinux/merizo_pipeline/merizo_analysis/results_parser.py",
+        search_file,
+    ]
     logger.info(f'STEP 2: RUNNING PARSER: {" ".join(cmd)}')
     run_command(cmd)
 
+
 def run_merizo_search(file_name, file_content):
     logger.info(f"File Name: {file_name}")
-    # Setting matplotlib config env var to a writable tmp directory 
+    # Setting matplotlib config env var to a writable tmp directory
     # since it's a merizo search dependency
-    os.environ['MPLCONFIGDIR'] = '/tmp/matplotlib_config'
-    # Create a temporary file to hold the pdb content 
+    os.environ["MPLCONFIGDIR"] = "/tmp/matplotlib_config"
+    # Create a temporary file to hold the pdb content
     # since merizo search requires the input to be a physical file
-    with NamedTemporaryFile(delete=True, mode='wb') as temp_file:
+    with NamedTemporaryFile(delete=True, mode="wb") as temp_file:
         temp_file.write(file_content)
         temp_file_path = temp_file.name
-        cmd = ['python3',
-           '/home/almalinux/merizo_search/merizo_search/merizo.py',
-           'easy-search',
-           temp_file_path,
-           '/home/almalinux/data/cath-4.3-foldclassdb',
-           file_name,
-           'tmp',
-           '--iterate',
-           '--output_headers',
-           '-d',
-           'cpu',
-           '--threads',
-           '2'
+        cmd = [
+            "python3",
+            "/home/almalinux/merizo_search/merizo_search/merizo.py",
+            "easy-search",
+            temp_file_path,
+            "/home/almalinux/data/cath-4.3-foldclassdb",
+            file_name,
+            "tmp",
+            "--iterate",
+            "--output_headers",
+            "-d",
+            "cpu",
+            "--threads",
+            "1",
         ]
         logger.info(f'STEP 1: RUNNING MERIZO: {" ".join(cmd)}')
-        p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
+        p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         _, err = p.communicate()
-        logger.info(f"Command {''.join(cmd)} Output: \n{err.decode('utf-8')}") # Using stderr since merizo writes everything to stderr
+        logger.info(
+            f"Command {''.join(cmd)} Output: \n{err.decode('utf-8')}"
+        )  # Using stderr since merizo writes everything to stderr
         if p.returncode != 0:
             logger.error(f"Command {''.join(cmd)} Error: \n{err.decode('utf-8')}")
 
@@ -71,23 +94,23 @@ def read_parsed_file(file_name):
     Reads a .parsed file and extracts:
     1. A dictionary with 'cath_id' as keys and their counts as values.
     2. The mean pLDDT value.
-    
+
     Args:
         file_path (str): Path to the .parsed file.
-        
+
     Returns:
         tuple: (dict of cath_id counts, mean pLDDT value)
     """
-    file_path = file_name + '.parsed'
+    file_path = file_name + ".parsed"
     if not os.path.exists(file_path):
-        return None, None # Skipping unsegmentable pdb files
-    
+        return None, None  # Skipping unsegmentable pdb files
+
     cath_counts = {}
     mean_plddt = 0.0
 
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         lines = file.readlines()
-        
+
         # Extract mean pLDDT from the header
         if len(lines) < 1:
             return cath_counts, mean_plddt
@@ -95,14 +118,14 @@ def read_parsed_file(file_name):
         first_line = lines[0]
         if "mean plddt:" in first_line:
             mean_plddt = float(first_line.split("mean plddt:")[1].strip())
-        
+
         # Skip the header and process the data rows
         for line in lines[2:]:  # Assuming data rows start from the 3rd line
             if not line.strip():
-                continue # Ignore empty lines
-            cath_id, count = line.strip().split(',')
+                continue  # Ignore empty lines
+            cath_id, count = line.strip().split(",")
             cath_counts[cath_id] = int(count)
-    
+
     return mean_plddt, cath_counts
 
 
@@ -116,4 +139,4 @@ def pipeline(file_tuple):
     mean_plddt, cath_counts_dict = read_parsed_file(file_name)
     # STEP 4
     upload_analysis_outputs_to_hdfs(file_name)
-    return mean_plddt, cath_counts_dict 
+    return mean_plddt, cath_counts_dict
